171b50043efc5cbeea2903062d78a29e0c7146b0
Cynthia59 <cindydai59@163.com>
2018-06-13 15:07:00 +0800

    增加复制最新报告的功能

--- webtest/testcase/models/function.py
+++ webtest/testcase/models/function.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-import os
+import os, shutil
 
 # 截图函数
 def insert_img(driver, file_name):
@@ -15,17 +15,27 @@ def latest_report():
     base_dir = os.path.dirname(os.path.dirname(__file__))
     base_dir = str(base_dir)
     base_dir = base_dir.replace('\\', '/')
-    base = base_dir.split('/test_case')[0]
+    base = base_dir.split('/testcase')[0]
     result_dir = base + "/report"
     lists = os.listdir(result_dir)
     # 重新按时间对目录下的文件进行排序
     lists.sort(key=lambda fn: os.path.getmtime(result_dir+"\\"+fn))
     print('最新的文件为： ' + lists[-1])
-    file = os.path.join(result_dir, lists[-1])
+    file = os.path.join(result_dir, lists[-1]).replace('\\','/')
     print(file)
+    return file
+
+#复制最新的测试报告
+def copy_latest_report():
+    old = latest_report()
+    new = old.split('/20')[0] + '/new_report.html'
+    shutil.copyfile(old, new)
+    print('copy done!')
+
     
 if __name__ == '__main__':
     driver = webdriver.Chrome()
     driver.get("http://www.baidu.com")
     insert_img(driver, 'baidu')
     driver.quit()
+    copy_latest_report()
